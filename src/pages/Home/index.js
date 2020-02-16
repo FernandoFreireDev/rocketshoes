b859474a8b7e4e440d0efd4396b4ddf13cc5f800
Fernando Freire <fernando.freire@bemobi.com.br>
2020-02-16 18:51:24 -0300

    adicionando ao carrinho

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { MdShoppingCart } from 'react-icons/md';
+import { connect } from 'react-redux';
 
 import api from '../../services/api';
 import { ProductList } from './styles';
 import { formatPrice } from '../../util/format';
 
-export default class Home extends Component {
+class Home extends Component {
   state = {
     products: [],
   }
@@ -21,6 +22,15 @@ export default class Home extends Component {
     this.setState({ products: data });
   }
 
+  handleAddProduct = product => {
+    const { dispatch } = this.props;
+
+    dispatch({
+      type: 'ADD_TO_CART',
+      product,
+    });
+  }
+
   render() {
     const { products } = this.state;
 
@@ -32,7 +42,7 @@ export default class Home extends Component {
             <strong>{product.title}</strong>
             <span>{product.priceFormatted}</span>
 
-            <button type="button">
+            <button type="button" onClick={() => this.handleAddProduct(product)}>
               <div>
                 <MdShoppingCart size={16} color="#fff" /> 3
               </div>
@@ -45,3 +55,5 @@ export default class Home extends Component {
     );
   }
 }
+
+export default connect()(Home);
--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -1,3 +1,10 @@
-export default function cart() {
-  return [];
+export default function cart(state = [], action) {
+  switch (action.type) {
+    case 'ADD_TO_CART':
+      return [ ...state, action.product ];
+      break;
+    default:
+      return state;
+      break;
+  }
 }
