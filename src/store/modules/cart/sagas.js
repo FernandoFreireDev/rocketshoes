499e7dc8b4db8d73a7faf96a6a3a8684f2721b1d
Fernando Freire <fernando.freire@bemobi.com.br>
2020-02-26 18:19:27 -0300

    verificando estoque na adiçao

--- src/store/modules/cart/sagas.js
+++ src/store/modules/cart/sagas.js
@@ -11,9 +11,19 @@ function* addToCart({ id }) {
     )
   );
 
-  if(productExist) {
-    const amount = productExist.amount + 1;
+  const stock = yield call(api.get, `/stock/${id}`);
 
+  const stockAmount = stock.data.amount;
+  const currentAmount = productExist ? productExist.amount : 0;
+
+  const amount = currentAmount + 1;
+
+  if(amount > stockAmount){
+    console.tron.warn('Error');
+    return;
+  }
+
+  if(productExist) {
     yield put(updateAmount(id, amount));
   } else {
     const response = yield call(api.get, `/products/${id}`);
